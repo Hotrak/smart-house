afaa4a309e68a7822c6595ca2d68debd430f1258
v.tumanovski <vladtumen2000@mail.ru>
2023-08-26 18:15:04 +0300

    update

--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require('cors')
 
 const PORT = process.env.PORT || 3000;
+
+app.use(cors())
 
 app.get("/api", (request, response) => {
     response.send("777");
