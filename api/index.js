2f6531dd1f77448623edce6df0152eb8b22aaf0d
v.tumanovski <vladtumen2000@mail.ru>
2023-08-26 17:59:42 +0300

    update

--- api/index.js
+++ api/index.js
@@ -6,5 +6,5 @@ const PORT = process.env.PORT || 3000;
 app.get("/api", (request, response) => {
     response.send("777");
 });
-module.exports = app;
-// app.listen(PORT);
+// module.exports = app;
+app.listen(PORT);
